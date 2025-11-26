03493c9620369aaaee69960f70f24a80a00cdc11
Alberto Castañeiras <alberto@vekio.dev>
2025-11-26 17:23:45 +0100

    chore: create flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "homelab-infra dev env";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "homelab-infra";
+
+          packages = with pkgs; [ ansible ];
+        };
+      });
+}
